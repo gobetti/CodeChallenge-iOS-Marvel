b7186bee67865108f6c5e27bcbcebce211377b48
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-12 18:50:05 -0300

    Added network models
    For now I didn't care for organizing in files or taking only what I
    really need from the models. They were auto-generated using
    https://quicktype-app.firebaseapp.com with only a few minor tweaks like
    adjusting struct names according to the docs and adding a date
    formatter.

---
+++ Marvel/API/MarvelModels.swift
@@ -0,0 +1,120 @@
+//
+//  MarvelModels.swift
+//  Marvel
+//
+//  Created by Marcelo Gobetti on 12/10/18.
+//
+
+import Foundation
+
+struct CharacterDataWrapper: Codable {
+    let code: Int?
+    let status, copyright, attributionText, attributionHTML, etag: String?
+    let data: CharacterDataContainer?
+
+    static let decoder: JSONDecoder = {
+        $0.dateDecodingStrategy = .formatted(Character.dateFormatter)
+        return $0
+    }(JSONDecoder())
+}
+
+struct CharacterDataContainer: Codable {
+    let offset, limit, total, count: Int?
+    let results: [Character]?
+}
+
+struct Character: Codable {
+    let id: Int?
+    let name: String?
+    let description: String?
+    let modified: Date?
+    let thumbnail: Image?
+    let resourceURI: String?
+    let comics: ComicList?
+    let stories: StoryList?
+    let events: EventList?
+    let series: SeriesList?
+    let urls: [MarvelURL]?
+
+    static let dateFormatter: DateFormatter = {
+        $0.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return $0
+    }(DateFormatter())
+}
+
+struct ComicList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [ComicSummary]?
+}
+
+struct ComicSummary: Codable {
+    let resourceURI: String?
+    let name: String?
+}
+
+struct StoryList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [StorySummary]?
+}
+
+struct StorySummary: Codable {
+    let resourceURI: String?
+    let name: String?
+    let type: StoryType?
+}
+
+enum StoryType: String, Codable {
+    case cover
+    case empty = ""
+    case interiorStory
+    case pinUp = "pinup"
+}
+
+struct EventList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [EventSummary]?
+}
+
+struct EventSummary: Codable {
+    let resourceURI: String?
+    let name: String?
+}
+
+struct SeriesList: Codable {
+    let available: Int?
+    let returned: Int?
+    let collectionURI: String?
+    let items: [SeriesSummary]?
+}
+
+struct SeriesSummary: Codable {
+    let resourceURI: String?
+    let name: String?
+}
+
+struct Image: Codable {
+    let path: String?
+    let `extension`: ImageExtension?
+}
+
+enum ImageExtension: String, Codable {
+    case gif
+    case jpg
+}
+
+struct MarvelURL: Codable {
+    let type: MarvelURLType?
+    let url: String?
+}
+
+enum MarvelURLType: String, Codable {
+    case comicLink = "comiclink"
+    case detail
+    case wiki
+}
--- MarvelTests/MarvelTests.swift
+++ MarvelTests/MarvelTests.swift
@@ -8,4 +8,15 @@
 import XCTest
 @testable import Marvel
 
-class MarvelTests: XCTestCase {}
+class MarvelTests: XCTestCase {
+    func testCharactersDecodingSucceeds() {
+        let bundle = Bundle(for: type(of: self))
+        let jsonFileURL = bundle.url(forResource: "characters_sample", withExtension: "json")!
+        let jsonData = try! Data(contentsOf: jsonFileURL)
+        let swiftType = CharacterDataWrapper.self
+        let decoder = swiftType.decoder
+        let dataWrapper = try! decoder.decode(swiftType, from: jsonData)
+
+        XCTAssertGreaterThan(dataWrapper.data!.results!.count, 0)
+    }
+}
