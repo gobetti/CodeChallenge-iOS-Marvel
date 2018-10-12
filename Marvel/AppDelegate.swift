09f49f54852f4994d538a52906e827a88a9ff6da
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-12 17:02:26 -0300

    Prevents unit tests from running UI code

--- Marvel/AppDelegate.swift
+++ Marvel/AppDelegate.swift
@@ -9,12 +9,21 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        self.window = window
+
+        if NSClassFromString("XCTest") != nil {
+            // Running unit tests, let's prevent UI from loading and concurring
+            window.rootViewController = UIViewController()
+        } else {
+            let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateInitialViewController()!
+            window.rootViewController = UINavigationController(rootViewController: viewController)
+        }
+        window.makeKeyAndVisible()
+
         return true
     }
 
