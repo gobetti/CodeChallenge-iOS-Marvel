b28b3cde480b3d9e188e14c838eda86496f69365
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-12 16:45:39 -0300

    Empty Xcode project with default settings

---
+++ Marvel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Marvel
+//
+//  Created by Marcelo Gobetti on 12/10/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
