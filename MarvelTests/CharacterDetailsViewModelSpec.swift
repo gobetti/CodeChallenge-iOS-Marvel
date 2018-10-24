18bc11ba844920353b0dd97671ce3235e465c838
Marcelo Gobetti <mwgobetti@gmail.com>
2018-10-24 00:50:18 -0300

    Re-indented and renamed spec tests files

---
+++ MarvelTests/CharacterDetailsViewModelSpec.swift
@@ -0,0 +1,76 @@
+//
+//  CharacterDetailsViewModelSpec.swift
+//  MarvelTests
+//
+//  Created by Marcelo Gobetti on 14/10/18.
+//
+
+import Quick
+import Nimble
+import RxCocoaNetworking
+import RxNimble
+import RxSwift
+import RxTest
+@testable import Marvel
+
+class CharacterDetailsViewModelSpec: QuickSpec {
+    override func spec() {
+        describe("Comics") {
+            let comicSummary = ComicSummary(resourceURI: "foo")
+            let comicList = ComicList(items: [comicSummary])
+            let character = Character(id: 1, name: nil, comics: comicList)
+
+            let initialTime = 0
+            var disposeBag: DisposeBag!
+            var scheduler: TestScheduler!
+
+            beforeEach {
+                disposeBag = DisposeBag()
+                scheduler = TestScheduler(initialClock: initialTime, simulateProcessingDelay: false)
+            }
+
+            it("starts empty then loads") {
+                let delay = 10
+                let service = MarvelService(stubBehavior: .delayed(time: TimeInterval(delay), stub: .default),
+                                            scheduler: scheduler)
+                let sut = CharacterDetailsViewModel(service: service)
+
+                expect(sut.comics(from: character).map { $0.count }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(initialTime, 0),
+                        Recorded.next(initialTime + delay, character.comics!.items!.count),
+                        Recorded.completed(initialTime + delay)
+                        ]))
+            }
+
+            it("does not error out if service response fails") {
+                let service = MarvelService(stubBehavior: .immediate(stub: .error(SomeError.error)),
+                                            scheduler: scheduler)
+                let sut = CharacterDetailsViewModel(service: service)
+
+                expect(sut.comics(from: character).map { $0.count }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(initialTime, 0),
+                        Recorded.completed(initialTime)
+                        ]))
+            }
+
+            it("does not error out if character has no comics") {
+                let service = MarvelService(stubBehavior: .immediate(stub: .default),
+                                            scheduler: scheduler)
+                let sut = CharacterDetailsViewModel(service: service)
+
+                let emptyCharacter = Character(id: 1, name: nil, comics: nil)
+
+                expect(sut.comics(from: emptyCharacter).map { $0.count }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(initialTime, 0),
+                        Recorded.completed(initialTime)
+                        ]))
+            }
+        }
+    }
+}
---
+++ MarvelTests/MarvelApiSpec.swift
@@ -0,0 +1,115 @@
+//
+//  MarvelApiSpec.swift
+//  MarvelTests
+//
+//  Created by Marcelo Gobetti on 12/10/18.
+//
+
+import Quick
+import Nimble
+import RxCocoaNetworking
+import RxNimble
+import RxSwift
+import RxTest
+@testable import Marvel
+
+extension MarvelApi: TargetType {
+    public var sampleData: Data {
+        switch self {
+        case .characters(let offset):
+            let jsonFileName: String
+            if offset == 0 {
+                jsonFileName = "characters_sample"
+            } else {
+                jsonFileName = "characters_offset20_sample"
+            }
+            
+            let jsonFileURL = urlForJsonFile(name: jsonFileName)
+            return try! Data(contentsOf: jsonFileURL)
+        case .comic:
+            let jsonFileURL = urlForJsonFile(name: "comic_sample")
+            return try! Data(contentsOf: jsonFileURL)
+        }
+    }
+    
+    private func urlForJsonFile(name: String) -> URL {
+        let bundle = Bundle(for: MarvelApiSpec.self)
+        return bundle.url(forResource: name, withExtension: "json")!
+    }
+}
+
+class MarvelApiSpec: QuickSpec {
+    override func spec() {
+        describe("Characters") {
+            let initialTime = 0
+            var disposeBag: DisposeBag!
+            var scheduler: TestScheduler!
+            
+            beforeEach {
+                disposeBag = DisposeBag()
+                scheduler = TestScheduler(initialClock: initialTime, simulateProcessingDelay: false)
+            }
+            
+            it("decodes successfully") {
+                let service = MarvelService(stubBehavior: .immediate(stub: .default), scheduler: scheduler)
+                
+                expect(service.characters().map { $0.count }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(initialTime, 20),
+                        Recorded.completed(initialTime)
+                        ]))
+            }
+            
+            it("is empty when wrapper misses data") {
+                let jsonData = """
+                {
+                  "code": 200,
+                  "status": "Ok",
+                  "copyright": "© 2018 MARVEL",
+                  "attributionText": "Data provided by Marvel. © 2018 MARVEL",
+                  "attributionHTML": "",
+                  "etag": "ac7a21e8b0ec4f96cb58754d74f342706661b037"
+                }
+                """.data(using: .utf8)!
+                let service = MarvelService(stubBehavior: .immediate(stub: .success(jsonData)), scheduler: scheduler)
+                
+                expect(service.characters().map { $0.count }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.next(initialTime, 00),
+                        Recorded.completed(initialTime)
+                        ]))
+            }
+        }
+        
+        describe("Comic") {
+            let initialTime = 0
+            var disposeBag: DisposeBag!
+            var scheduler: TestScheduler!
+            
+            beforeEach {
+                disposeBag = DisposeBag()
+                scheduler = TestScheduler(initialClock: initialTime, simulateProcessingDelay: false)
+            }
+            
+            it("errors out when wrapper has no results") {
+                let jsonData = """
+                {
+                    "data": {
+                        "results": []
+                    }
+                }
+                """.data(using: .utf8)!
+                let service = MarvelService(stubBehavior: .immediate(stub: .success(jsonData)),
+                                            scheduler: scheduler)
+                
+                expect(service.comic(resourceURI: "").map { $0.id! }.asObservable())
+                    .events(scheduler: scheduler, disposeBag: disposeBag)
+                    .to(equal([
+                        Recorded.error(initialTime, MarvelServiceError.noComic)
+                        ]))
+            }
+        }
+    }
+}
